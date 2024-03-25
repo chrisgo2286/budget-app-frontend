05310cbab53ccfaed83513dfc0f2bc9410b21ea4
Christian Go <chrisgo2286@yahoo.com>
2024-03-24 20:51:35 -0400

    Added styling for navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import './navbar.css';
 
 export default function Navbar () {
     return (
         <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/budget'>Budget</Link>
-            <Link to='/ledger'>Ledger</Link>
+            <section className='site-links'>
+                <Link to='/'>Home</Link>
+                <Link to='/budget'>Budget</Link>
+                <Link to='/ledger'>Ledger</Link>
+            </section>
+            <section className='user-links'>
+                <div className="greeting">Hello, Christian</div>
+                <a href='#'>Logout</a>
+            </section>
         </nav>
     )
 }
