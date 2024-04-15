0ae62353c6c9fa665a0a7d21debe53719e63a9f5
Christian Go/CAN <christian.go@ca.cushwake.com>
2024-04-15 15:14:15 -0600

    Completed validation.test.js

--- src/misc/validation/validation.test.js
+++ src/misc/validation/validation.test.js
@@ -32,6 +32,70 @@ describe('validateYear', () => {
 
 describe('validateCategory', () => {
     const validCategories = [
-        ['']
+        ['Salary', 'Valid'],
+        ['Grocery', 'Valid']
     ]
+    const invalidCategories = [
+        ['', 'Please enter a category!'],
+        ['Category', 'Please choose a valid category!']
+    ]
+    it.each(validCategories)('Valid categories return as valid', (string, result) => {
+        expect(validateCategory(string)).toBe(result);
+    })
+
+    it.each(invalidCategories)('Invalid categories return error statement', (string, result) => {
+        expect(validateCategory(string)).toBe(result);
+    });
+})
+
+describe('validateType', () => {
+    const validTypes = [
+        ['Expense', 'Valid'],
+        ['Income', 'Valid'],
+    ]
+    it.each(validTypes)('Valid types return as valid', (string, result) => {
+        expect(validateType(string)).toBe(result);
+    });
+
+    it('Returns error msg if type is Type', () => {
+        expect(validateType('Type')).toBe('Please enter a valid type!');
+    });
+})
+
+describe('validateAmount', () => {
+    const validAmounts = [
+        ['1000', 'Valid'],
+        ['100', 'Valid'],
+        ['100.00', 'Valid'],
+        ['100.0', 'Valid']
+    ]
+    const invalidAmounts = [
+        ['abcd', 'Please enter a valid amount!'],
+        ['100a', 'Please enter a valid amount!'],
+        ['$100', 'Please enter a valid amount!'],
+        ['1,000', 'Please enter a valid amount!'],
+        ['100,00', 'Please enter a valid amount!'],
+        ['', 'Please enter an amount!']
+    ]
+    it.each(validAmounts)('Valid amount returns as valid', (string, result) => {
+        expect(validateAmount(string)).toBe(result);
+    });
+
+    it.each(invalidAmounts)('Invalid amount returns error msg', (string, result) => {
+        expect(validateAmount(string)).toBe(result);
+    });
+})
+
+describe('validateFilterDates', () => {
+    const invalidFilterDates =  [
+        [[new Date('4/30/2024'), new Date('4/1/2024')], false],
+        [[new Date('4/1/2024'), new Date('4/1/2024')], false]
+    ]
+    it('Returns as valid if end date > start date', () => {
+        expect(validateFilterDates(new Date('1/1/2024'), new Date('3/31/2024')))
+        .toBe('Valid');
+    });
+    it.each(invalidFilterDates, (strings, result) => {
+        expect(validateFilterDates())
+    })
 })
