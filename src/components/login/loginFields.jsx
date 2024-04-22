1ae270d1e164945f2caf17e8eff74d989c4ba6b0
Christian Go <chrisgo2286@yahoo.com>
2024-04-22 17:17:38 -0400

    Added data-cy tags for login page

--- src/components/login/loginFields.jsx
+++ src/components/login/loginFields.jsx
@@ -4,23 +4,38 @@ import Input from "../miscComponents/input/input";
 export default function LoginFields ({ fields, setFields, handleSubmit }) {
     return (
         <React.Fragment>
-            <div className="login-username-label">Username</div>
+            <div 
+                className="login-username-label"
+                data-cy='login-username-label'>
+                Username
+            </div>
             <Input
                 className='login-username'
                 type='text'
                 name='username'
                 value={ fields.username }
                 fields={ fields }
-                setFields={ setFields } />
-            <div className="login-password-label">Password</div>
+                setFields={ setFields }
+                data-cy='login-username' />
+            <div 
+                className="login-password-label"
+                data-cy='login-password-label'>
+                Password
+            </div>
             <Input
                 className='login-password'
                 type='password'
                 name='password'
                 value={ fields.password }
                 fields={ fields }
-                setFields={ setFields } />
-            <button className='login-btn' onClick={ handleSubmit }>Login</button>
+                setFields={ setFields }
+                data-cy='login-password' />
+            <button 
+                className='login-btn' 
+                onClick={ handleSubmit }
+                data-cy='login-btn'>
+                Login
+            </button>
         </React.Fragment>
     )
 }
