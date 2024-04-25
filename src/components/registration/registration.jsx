3e44026610a20b743ba88cd2cb83b265e64690e3
Christian Go <chrisgo2286@yahoo.com>
2024-04-25 17:19:02 -0400

    Added cypress testing for registration

--- src/components/registration/registration.jsx
+++ src/components/registration/registration.jsx
@@ -4,8 +4,10 @@ import { UserContext } from '../../misc/context';
 import { registerNewUser } from '../../misc/apiCalls';
 import { updateLocalStorage } from '../../misc/userFunctions';
 import { updateUser } from '../../misc/userFunctions';
-import NewUserFields from './newUserFields';
 import { loginUser } from '../../misc/apiCalls';
+import { validateRegistration } from '../../misc/validation/validateRegistration';
+import NewUserFields from './newUserFields';
+import Validation from '../validation/validation';
 import './registration.css';
 
 export default function Registration () {
@@ -16,36 +18,44 @@ export default function Registration () {
         password1: '',
         password2: '',
     })
+    const [errors, setErrors] = useState([]);
 
     async function handleSubmit () {
 
-        const response = await registerNewUser(credentials);
-        
-        if(response.status === 201) {
-            const token = response.data.key;
-            updateLocalStorage(token, credentials.username)
-            updateUser(token, credentials.username, user, setUser)
-            navigate('/');
+        const result = validateRegistration(credentials);
 
-        } else if(response.status === 204) {
-            const loginCredentials = {
-                username: credentials.username,
-                password: credentials.password1
-            }
-
-            const response = await loginUser(loginCredentials);
-
-            if(response.status && response.status === 200) {
+        if(result === 'Valid') {
+            const response = await registerNewUser(credentials);
+            
+            if(response.status === 201) {
                 const token = response.data.key;
-                updateLocalStorage(token, credentials.username);
-                updateUser(token, credentials.username, user, setUser);
+                updateLocalStorage(token, credentials.username)
+                updateUser(token, credentials.username, user, setUser)
                 navigate('/');
+
+            } else if(response.status === 204) {
+                const loginCredentials = {
+                    username: credentials.username,
+                    password: credentials.password1
+                }
+
+                const response = await loginUser(loginCredentials);
+
+                if(response.status && response.status === 200) {
+                    const token = response.data.key;
+                    updateLocalStorage(token, credentials.username);
+                    updateUser(token, credentials.username, user, setUser);
+                    navigate('/');
+                }
             }
-        }    
+        } else {
+            setErrors(result);
+        }
     }
 
     return (
         <div className="registration" data-cy='registration'>
+            <Validation errors={ errors } />
             <NewUserFields 
                 fields={ credentials } 
                 setFields={ setCredentials }
