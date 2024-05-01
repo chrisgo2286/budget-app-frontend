074441016b120c2ce089e6cf8007ad26e554b198
Christian Go/CAN <christian.go@ca.cushwake.com>
2024-05-01 08:04:56 -0600

    Created folder for misc functions test

---
+++ src/misc/tests/miscFunctions.test.js
@@ -0,0 +1,16 @@
+import {
+    findCategoryID,
+} from '../miscFunctions';
+
+const categories = [
+    {name: 'Grocery'}, 
+    {name: 'Mortgage'}, 
+    {name: 'Clothing'}
+]
+
+describe('findCategoryID', () => {
+    const categoryNames = [
+        [''
+    ]
+    it('')
+})
