6c91bd750e4bf21105499a9027150ed1d9515ad7
Christian Go <chrisgo2286@yahoo.com>
2024-06-19 23:46:04 -0400

    Worked on PieChart

--- src/components/reports/pieChart.jsx
+++ src/components/reports/pieChart.jsx
@@ -3,17 +3,22 @@ import { Pie } from "react-chartjs-2";
 export default function PieChart ({ budget }) {
 
     const data = {
-        labels: budget.map((item) => item.category),
+        labels: validCategories().map((item) => item.category),
         datasets: [
             {
                 label: "Monthly Expenditure",
-                data: budget.map((item) => item.actual_amount),
+                data: validCategories().map((item) => item.actual_amount),
                 backgroundColor: ["blue", "red", "orange"],
                 borderColor: "black",
                 borderWidth: 2
             }
         ]
     }
+
+    function validCategories () {
+        return budget.filter((item) => item.actual_amount > 0)
+    }
+
     return (
         <div className="pie-chart">
             <Pie
