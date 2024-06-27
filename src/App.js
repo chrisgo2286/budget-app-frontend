994983cdb80f2cd01f1ae8e533e1d068e46a9e25
Christian Go <chrisgo2286@yahoo.com>
2024-06-27 16:47:45 -0400

    Changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/home';
 import Navbar from './components/navbar/navbar';
 import Budget from './components/budget/budget';
